72f29cd4014226622f9244a75c5d451fdce9e77d
GabrielGameDev <19438299+GabrielGameDev@users.noreply.github.com>
2018-12-26 21:19:29 -0200

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour {
+
+	public float speed = 5;
+
+	private float mouseDistance;
+	private Rigidbody2D rb;
+
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		float xPos = worldPoint.x;
+
+		mouseDistance = Mathf.Clamp(xPos - transform.position.x, -1, 1);
+
+	}
+
+	private void FixedUpdate()
+	{
+		rb.velocity = new Vector2(mouseDistance * speed, LevelController.instance.gameSpeed);
+	}
+}
