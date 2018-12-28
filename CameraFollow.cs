e6442b942adc6c61fd0cd9443d107fc5b36398e6
GabrielGameDev <19438299+GabrielGameDev@users.noreply.github.com>
2018-12-28 12:55:35 -0200

    Add files via upload

---
+++ CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+
+		offset = transform.position - player.transform.position;
+
+	}
+
+	private void LateUpdate()
+	{
+		transform.position = new Vector3(0, player.position.y + offset.y, player.position.z + offset.z);
+	}
+}
