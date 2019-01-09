797ec2597d7f2e99c2478de4ee36275832b1cf3c
GabrielGameDev <19438299+GabrielGameDev@users.noreply.github.com>
2019-01-09 10:03:36 -0200

    Add files via upload

---
+++ Pickup.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pickup : MonoBehaviour {
+
+	public Text amountText;
+	public GameObject bodyPrefab;
+
+	private int amount;
+
+	private void OnEnable()
+	{
+		amount = Random.Range(1, 6);
+		amountText.text = amount.ToString();
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			for (int i = 0; i < amount; i++)
+			{
+				int index = other.transform.childCount;
+				GameObject newBody = Instantiate(bodyPrefab, other.transform);
+				newBody.transform.localPosition = new Vector3(0, -index, 0);
+
+				FollowTarget followTarget = newBody.GetComponent<FollowTarget>();
+				if(followTarget != null)
+				{
+					followTarget.target = other.transform.GetChild(index - 1);
+				}
+			}
+		}
+	}
+}
